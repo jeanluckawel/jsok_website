503aeec23e53e63b7172e583f6eec1963c6f8bf6
kafandapaul <paulkaf84@gmail.com>
2024-06-03 22:24:09 +0200

    fix ui, add Ajouter au panier

--- resources/js/Pages/Layout.jsx
+++ resources/js/Pages/Layout.jsx
@@ -1,19 +1,29 @@
 import {Head} from "@inertiajs/react";
 import MainNav from "./MainNav.jsx";
 import "../../css/app.css"
-import {Drawer} from "antd";
+import {Drawer, Modal} from "antd";
 import {useState} from "react";
 import CartItem from "./CartItem.jsx";
 
 export default function Layout({children}){
 
     const [open, setOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     function showDrawer() {
         setOpen(true)
     }
 
     const onClose = () => {
         setOpen(false);
+    };
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
     };
 
     return <>
@@ -44,11 +54,56 @@ export default function Layout({children}){
 
                 <section className="absolute bottom-4 w-full">
                     <div className="w-full">
-                        <button className="flex justify-center items-center space-x-2 min-w-[21rem] p-2 bg-blue-500 rounded font-bold text-white"><span className="material-symbols-outlined">shopping_cart</span> <span>Checkout</span>
+                        <button onClick={showModal}
+                                className="flex justify-center items-center space-x-2 min-w-[21rem] p-2 bg-blue-500 rounded font-bold text-white">
+                            <span className="material-symbols-outlined">shopping_cart</span> <span>Checkout</span>
                         </button>
                     </div>
                 </section>
             </Drawer>
+        </section>
+
+        <section>
+            <Modal
+                open={isModalOpen}
+                onCancel={handleCancel}
+                onOk={handleOk}
+                title="Your Information's"
+            >
+                <form action="#" className="container mx-auto flex flex-col space-y-6 mt-8 w-full mb-8">
+                    <div className="flex flex-col space-y-2">
+                        <label htmlFor="name" className="inline-flex items-center">Your Name <span className="ml-1 text-red-500 font-semibold">*</span></label>
+                        <div className="flex items-center w-full border rounded">
+                            <div className="flex items-center p-2 border-r bg-gray-50 text-gray-600">
+                                <span className="material-symbols-outlined">account_circle</span>
+                            </div>
+                            <input className="w-full rounded-r outline-none px-4 py-2"
+                                type="text" placeholder="John Doe" id="name" required/>
+                        </div>
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <label htmlFor="phone" className="inline-flex items-center">Your Phone Number <span className="ml-1 text-red-500 font-semibold">*</span></label>
+                        <div className="flex items-center w-full border rounded">
+                            <div className="flex items-center p-2 border-r bg-gray-50 text-gray-600">
+                                <span className="material-symbols-outlined">call</span>
+                            </div>
+                            <input className="w-full rounded-r outline-none px-4 py-2"
+                                type="text" id="phone" name="phone" placeholder="097 10 76 087" required/>
+                        </div>
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <label htmlFor="address" className="inline-flex items-center">Your Address <span className="ml-1 text-red-500 font-semibold">*</span></label>
+                        <div className="flex items-center w-full border rounded">
+                            <div className="flex items-center p-2 border-r bg-gray-50 text-gray-600">
+                                <span className="material-symbols-outlined">location_on</span>
+                            </div>
+                            <input className="w-full rounded-r outline-none px-4 py-2"
+                                type="text" id="address" name="address" placeholder="25 Matobo/Lido Golf" required/>
+                        </div>
+                    </div>
+                    <input type="submit" hidden={true}/>
+                </form>
+            </Modal>
         </section>
     </>
 }
--- resources/js/Pages/ProductCard.jsx
+++ resources/js/Pages/ProductCard.jsx
@@ -2,18 +2,19 @@ import {Image} from "antd";
 
 export default function ProductCard({Product, id}) {
     return(
-        <div className="w-full flex flex-col text-gray-700">
+        <div className="w-full flex flex-col space-y-2 text-gray-700 border rounded-t-2xl rounded-b-md p-2 shadow">
             <div>
-                <Image className="rounded-2xl"
+                <Image className="rounded-t-2xl rounded-b-md"
                        src={"https://picsum.photos/id/" + id + "/400"} alt="Product Image"/>
             </div>
-            <div className="mt-4 font-semibold">
+            <div className="font-semibold text-[0.9rem]">
                 <legend>
                     <span className="block leading-relaxed">Apple Mac Book Pro(M2)</span>
                     <span className="block leading-relaxed font-normal">Price: <span>2300$</span></span>
                     <span className="block leading-relaxed">Stock: <span>150 pc</span></span>
                 </legend>
             </div>
+            <button className="bg-amber-200 py-1 rounded-md text-gray-700 font-semibold hover:bg-amber-100 text-[0.9rem]">Ajouter au Panier</button>
         </div>
     )
 }
